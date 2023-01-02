1874fb9411af2b154cf37a19268ff885bd41caea
Badjatya <architj240@gmail.com>
2023-01-02 23:57:56 +0530

    [Feat]: Practicing with images

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const imgUrl =
+    "https://images.pexels.com/photos/13945391/pexels-photo-13945391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Hello World!</Text>
+            <View style={styles.imageContainer}>
+                <Image source={{ uri: imgUrl }} style={styles.image} />
+            </View>
+            <Text>Hello World</Text>
             <StatusBar style="auto" />
         </View>
     );
@@ -13,8 +19,16 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#25292e",
         alignItems: "center",
-        justifyContent: "center",
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
     },
 });

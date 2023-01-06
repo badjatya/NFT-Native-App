363106d762272be9d18078cadc6668539d4e0540
Badjatya <architj240@gmail.com>
2023-01-06 14:00:35 +0530

    [Chore]: Installed Library

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-
-const imgUrl =
-    "https://images.pexels.com/photos/13945391/pexels-photo-13945391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image source={{ uri: imgUrl }} style={styles.image} />
-            </View>
-            <Text>Hello World</Text>
-            <StatusBar style="auto" />
+        <View style={{ flex: 1 }}>
+            <Text>Hello</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#25292e",
-        alignItems: "center",
-    },
-    imageContainer: {
-        flex: 1,
-        paddingTop: 58,
-    },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
-    },
-});
+const styles = StyleSheet.create({});

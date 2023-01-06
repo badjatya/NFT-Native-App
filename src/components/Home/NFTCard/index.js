0338f50f20566af7aba96d419c7a27b2c710820f
Badjatya <architj240@gmail.com>
2023-01-06 14:56:51 +0530

    [Feat]: Components

---
+++ src/components/Home/NFTCard/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const NFTCard = () => {
+    return (
+        <View>
+            <Text>NFTCard</Text>
+        </View>
+    );
+};
+
+export default NFTCard;
+
+const styles = StyleSheet.create({});
---
+++ src/components/Layout/FocusedStatusBar/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const FocusedStatusBar = () => {
+    return (
+        <View>
+            <Text>FocusedStatusBar</Text>
+        </View>
+    );
+};
+
+export default FocusedStatusBar;
+
+const styles = StyleSheet.create({});

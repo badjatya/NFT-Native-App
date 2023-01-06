60b2de1fc0b5453b7a0e413d42289f339587f44d
Badjatya <architj240@gmail.com>
2023-01-06 14:17:51 +0530

    [Feat]: Setup for Stack Navigation

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens
+import Home from './src/screens/Home';
+import Details from './src/screens/Details';
+
+const Stack = createNativeStackNavigator();
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: 'transparent',
+    },
+};
 
 export default function App() {
     return (
-        <View style={{ flex: 1 }}>
-            <Text>Hello</Text>
-        </View>
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Details" component={Details} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({});

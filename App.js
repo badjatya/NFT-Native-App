513396421df3a2ac9846b5ed4888c3c27a8bea55
Badjatya <architj240@gmail.com>
2023-01-06 14:31:14 +0530

    [Feat]: Import Updated

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 
 // Screens
-import Home from './src/screens/Home';
-import Details from './src/screens/Details';
+import { Home, Details } from './src/screens';
 
+// Creating Stack Navigation
 const Stack = createNativeStackNavigator();
+
+// Theme for Navigation
 const theme = {
     ...DefaultTheme,
     colors: {
@@ -15,6 +18,19 @@ const theme = {
 };
 
 export default function App() {
+    // Loading Font
+    const [loaded] = useFonts({
+        InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+        InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+        InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+        InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+        InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    });
+
+    // If font not loaded returning null
+    if (!loaded) return null;
+
+    // Main
     return (
         <NavigationContainer theme={theme}>
             <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">

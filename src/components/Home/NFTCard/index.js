77645b57424ca1288563ab1f2619f4d71c356193
Badjatya <architj240@gmail.com>
2023-01-07 23:32:11 +0530

    [Feat]: Added Component import export

--- src/components/Home/NFTCard/index.js
+++ src/components/Home/NFTCard/index.js
@@ -1,9 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+// Importing Packages
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const NFTCard = () => {
+// Importing Components
+import { CircleButton } from '../../Layout';
+
+// Importing Constants
+import { COLORS, SIZES, SHADOWS, assets } from '../../../constants';
+
+const NFTCard = ({ data }) => {
     return (
-        <View>
+        <View style={styles.card}>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={data.image}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+                <CircleButton imgUrl={assets.heart} top={10} right={10} />
+            </View>
             <Text>NFTCard</Text>
         </View>
     );
@@ -11,4 +26,22 @@ const NFTCard = () => {
 
 export default NFTCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+    },
+    imageContainer: {
+        width: '100%',
+        height: 250,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderTopLeftRadius: SIZES.font,
+        borderTopRightRadius: SIZES.font,
+    },
+});
